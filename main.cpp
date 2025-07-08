bef862d5fb357e426657133b40b5b247953a48be
jrickelton <jjarickelton@gmail.com>
2025-07-08 16:39:52 +0100

    ♻️  Refactor game state management

--- main.cpp
+++ main.cpp
@@ -8,37 +8,40 @@ enum state_t
   PLAYING,
   GAME_OVER
 };
-state_t state = PLAYING;
-
-int score = 0;
-bool button_a_was_pressed = false;
 
 struct vec_t
 {
   int32_t x, y;
 };
 
-struct sound
+struct Game 
+{
+  state_t state = PLAYING;
+  bool button_a_was_pressed = false;
+  int32_t score = 0;
+  vec_t bounds = {18, 16};
+  int scale = 6;
+};
+
+Game game;
+
+struct Sound
 {
   voice_t v;
-  sound(int32_t attack, int32_t decay, int32_t sustain, int32_t release, int32_t bend = 0, int32_t bend_ms = 0, int32_t reverb = 0, int32_t noise = 0, int32_t distort = 0) : v(voice(attack, decay, sustain, release, bend, bend_ms, reverb, noise, distort)) {}
+  Sound(int32_t attack, int32_t decay, int32_t sustain, int32_t release, int32_t bend = 0, int32_t bend_ms = 0, int32_t reverb = 0, int32_t noise = 0, int32_t distort = 0) : v(voice(attack, decay, sustain, release, bend, bend_ms, reverb, noise, distort)) {}
   
   void play_sound(int32_t frequency, int32_t duration, int32_t volume = 100) {
     play(v, frequency, duration, volume);
   }
 };
 
-
-constexpr vec_t bounds{.x = 18, .y = 16};
-constexpr int scale = 6;
-
-struct
+struct Birb
 {
   vec_t dir;
   vec_t body = {1, 8};
   uint32_t last_update = 0;
-  sound flap_sound = sound(250, 100, 40, 0, 0, 0, 0, 0, 0);
-  sound collision_sound = sound(5, 500, 400, 600, 0, 0, 0, 0, 0);
+  Sound flap_sound = Sound(250, 100, 40, 0, 0, 0, 0, 0, 0);
+  Sound collision_sound = Sound(5, 500, 400, 600, 0, 0, 0, 0, 0);
 
   void flap(uint32_t tick)
   {
@@ -58,11 +61,11 @@ struct
   {
     return {.x = body.x + dir.x, .y = body.y + dir.y};
   }
-} birb;
+};
 
-struct
+struct Pipe
 {
-  int32_t x_pos = bounds.x;
+  int32_t x_pos = game.bounds.x;
   int32_t gap_size = 4;
   int32_t gap_pos = generate_gap_pos();
 
@@ -78,14 +81,14 @@ struct
     return {.x = x_pos, .y = gap_pos + gap_size};
   }
 
-  int32_t bottom_pipe_height = bounds.y - (gap_pos + gap_size);
+  int32_t bottom_pipe_height = game.bounds.y - (gap_pos + gap_size);
 
   void next()
   {
     x_pos -= 1;
     if (x_pos == 0)
     {
-      score++;
+      game.score++;
     }
     if (x_pos == -2)
     {
@@ -95,27 +98,33 @@ struct
 
   int32_t generate_gap_pos()
   {
-    return rand() % (bounds.y - gap_size);
+    return rand() % (game.bounds.y - gap_size);
   }
 
   void reset()
   {
-    x_pos = bounds.x;
+    x_pos = game.bounds.x;
     gap_pos = generate_gap_pos();
     top_pipe_height = gap_pos;
-    bottom_pipe_height = bounds.y - (gap_pos + gap_size);
+    bottom_pipe_height = game.bounds.y - (gap_pos + gap_size);
   }
-} pipe;
+};
+
+
+
 
 vec_t transform(vec_t v)
 {
-  return {.x = (v.x * scale) + 6, .y = (v.y * scale) + 18};
+  return {.x = (v.x * game.scale) + 6, .y = (v.y * game.scale) + 18};
 }
+
+Birb birb = Birb();
+Pipe pipe = Pipe();
 
 bool collision()
 {
-  return birb.body.y < 0 || birb.body.y >= bounds.y ||
-         birb.body.x < 0 || birb.body.x >= bounds.x ||
+  return birb.body.y < 0 || birb.body.y >= game.bounds.y ||
+         birb.body.x < 0 || birb.body.x >= game.bounds.x ||
          (birb.body.x == pipe.x_pos &&
           (birb.body.y < pipe.top_pipe_height - 1 ||
            birb.body.y > pipe.bottom_pipe_start().y));
@@ -123,8 +132,8 @@ bool collision()
 
 void init()
 {
-  state = PLAYING;
-  score = 0;
+  game.state = PLAYING;
+  game.score = 0;
 
   birb.dir = {.x = 0, .y = 1};
   birb.body = {.x = 1, .y = 8};
@@ -135,9 +144,9 @@ void init()
 
 void update(uint32_t tick)
 {
-  if (state == PLAYING)
+  if (game.state == PLAYING)
   {
-    if (!button_a_was_pressed && button(A))
+    if (!game.button_a_was_pressed && button(A))
     {
       birb.flap(tick);
     }
@@ -155,15 +164,15 @@ void update(uint32_t tick)
     if (collision())
     {
       birb.collision_sound.play_sound(200, 1000, 40);
-      state = GAME_OVER;
+      game.state = GAME_OVER;
     }
 
-    button_a_was_pressed = button(A);
+    game.button_a_was_pressed = button(A);
   }
   else
   {
     text("GAME OVER!", 15, 30);
-    text("Score: " + std::to_string(score), 15, 40);
+    text("Score: " + std::to_string(game.score), 15, 40);
     text("Press A to restart", 15, 50);
     // game over, if user presses A then restart
     if (pressed(A))
@@ -182,17 +191,17 @@ void draw(uint32_t tick)
   // Draw the birb
   pen(15, 14, 1);
   vec_t birb_screen_pos = transform(birb.body);              // Transform grid position to screen position
-  frect(birb_screen_pos.x, birb_screen_pos.y, scale, scale); // Draw the birb as a rectangle
+  frect(birb_screen_pos.x, birb_screen_pos.y, game.scale, game.scale); // Draw the birb as a rectangle
 
   // Draw the pipes
   pen(0, 255, 0); // Green color for pipes
   vec_t top_pipe_screen_pos = transform(pipe.top_pipe_start());
   vec_t bottom_pipe_screen_pos = transform(pipe.bottom_pipe_start());
-  frect(top_pipe_screen_pos.x, top_pipe_screen_pos.y, scale, pipe.top_pipe_height * scale);
-  frect(bottom_pipe_screen_pos.x, bottom_pipe_screen_pos.y, scale, (pipe.bottom_pipe_height + 1) * scale);
+  frect(top_pipe_screen_pos.x, top_pipe_screen_pos.y, game.scale, pipe.top_pipe_height * game.scale);
+  frect(bottom_pipe_screen_pos.x, bottom_pipe_screen_pos.y, game.scale, (pipe.bottom_pipe_height + 1) * game.scale);
 
   // draw the scoreboard
   pen(0, 0, 0, 4);
   hline(2, 12, 116);
-  text(std::to_string(score), 3, 3);
+  text(std::to_string(game.score), 3, 3);
 }
